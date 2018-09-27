59e915a1ac64021611809b66872d2e5bd4e51368
Clayton <cjdetke@yahoo.com>
2018-09-27 18:49:43 -0400

    Initial commit
    This is incredibly basic, we have a lot to do from here!

---
+++ Assets/C#/FollowingCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowingCamera : MonoBehaviour {
+    
+    GameObject[] targets;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        targets = GameObject.FindGameObjectsWithTag("Player");
+
+        if (targets != null)
+        {
+            GameObject target = targets[0];
+            transform.position = new Vector3(target.transform.position.x, this.transform.position.y, this.transform.position.z);
+        }
+    }
+}
---
+++ Assets/C#/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public int movementSpeed = 3000;
+    public int maxHorizontalVelocity = 200;
+    public int jumpHeight = 400;
+    private Rigidbody2D myRigid;
+    private bool canJump;
+    private int jumpCounter, maxJumps = 2;
+
+	// Use this for initialization
+	void Start () {
+        myRigid = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontalMovement = movementSpeed * Input.GetAxis("Horizontal");
+        if((horizontalMovement > 0 && myRigid.velocity.x < maxHorizontalVelocity) || (horizontalMovement < 0 && myRigid.velocity.x > -maxHorizontalVelocity))
+        {
+            myRigid.AddForce(new Vector2(horizontalMovement * Time.deltaTime, 0));
+        }
+        if (Input.GetAxisRaw("Vertical") > 0 && canJump && jumpCounter < maxJumps)
+        {
+            myRigid.AddForce(new Vector2(0, jumpHeight));
+            jumpCounter++;
+        }
+        else if (Input.GetAxisRaw("Vertical") < 0)
+        {
+            //TODO: add crouching functionality
+        }
+        canJump = Input.GetAxis("Vertical") <= 0;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        jumpCounter = 0;
+    }
+}
