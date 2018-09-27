f549b3b4ad39ad2f0035fe679b50201d399bbcad
Landon Ellis <landon.ellis2@gmail.com>
2018-09-27 19:31:26 -0400

    Initial Commit

--- Assets/C#/PlayerMovement.cs
+++ Assets/C#/PlayerMovement.cs
@@ -3,22 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
+
     public int movementSpeed = 3000;
     public int maxHorizontalVelocity = 200;
     public int jumpHeight = 400;
+
     private Rigidbody2D myRigid;
+    private BoxCollider2D myBox;
+
+    private bool isCrouching = false;
+
     private bool canJump;
     private int jumpCounter, maxJumps = 2;
 
 	// Use this for initialization
 	void Start () {
         myRigid = this.GetComponent<Rigidbody2D>();
+        myBox = this.GetComponent<BoxCollider2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         float horizontalMovement = movementSpeed * Input.GetAxis("Horizontal");
-        if((horizontalMovement > 0 && myRigid.velocity.x < maxHorizontalVelocity) || (horizontalMovement < 0 && myRigid.velocity.x > -maxHorizontalVelocity))
+        if((horizontalMovement > 0 && myRigid.velocity.x < maxHorizontalVelocity)
+            || (horizontalMovement < 0 && myRigid.velocity.x > -maxHorizontalVelocity))
         {
             myRigid.AddForce(new Vector2(horizontalMovement * Time.deltaTime, 0));
         }
@@ -30,6 +38,13 @@ public class PlayerMovement : MonoBehaviour {
         else if (Input.GetAxisRaw("Vertical") < 0)
         {
             //TODO: add crouching functionality
+            if (!isCrouching)
+            {
+
+                
+
+            }
+
         }
         canJump = Input.GetAxis("Vertical") <= 0;
     }
