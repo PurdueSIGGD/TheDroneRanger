8de88f3d6bd7c016a61a43b2f9962ae5dd503f30
Jordan <jorda128@purdue.lcl>
2018-09-29 18:24:43 -0400

    core - constructor for PlayerAttributes

--- Assets/C#/PlayerAttributes.cs
+++ Assets/C#/PlayerAttributes.cs
@@ -1,3 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+
+public class PlayerAttributes {
+    private double health;
+    //public Weapon currentWeapon;
+
+    //high noon at 1, should not be > 1
+    private double highNoonPercent;
+    
+    //may not be needed, depending on what gets put in Weapon class
+    private int ammoCount;
+
+    //default constructor
+    public PlayerAttributes() {
+        health = 100.0;
+        highNoonPercent = 0.0;
+        ammoCount = 6;
+    }
+
+    //constructor
+    public PlayerAttributes(double health, double highNoonPercent, int ammoCount) {
+        this.health = health;
+        this.highNoonPercent = highNoonPercent;
+        this.ammoCount = ammoCount;
+    }
+}
