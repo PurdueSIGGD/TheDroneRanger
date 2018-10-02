b5ea36152cdeec66d8a4ea4e7903731deadac63c
Clayton <cjdetke@yahoo.com>
2018-10-02 19:07:01 -0400

    Added comments to my code

--- Assets/C#/FollowingCamera.cs
+++ Assets/C#/FollowingCamera.cs
@@ -6,15 +6,22 @@ public class FollowingCamera : MonoBehaviour {
 
     GameObject[] targets;
     GameObject target;
+
+    // Determines the position of the player on the screen (with regards to the camera)
     public float offsetX = 0, offsetY = 0;
+    // Determines whether or not the camera will follow the player as he traverses the Y-axis
     bool followYAxis = false;
+
     private float initY, initZ;
 
     // Use this for initialization
     void Start()
     {
+
+        // Determine the player to have the camera follow
         targets = GameObject.FindGameObjectsWithTag("Player");
         target = targets[0];
+        // Get the initial position of the camera
         initY = this.transform.position.y;
         initZ = this.transform.position.z;
     }
@@ -27,6 +34,7 @@ public class FollowingCamera : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        // Move the camera based off of the change in the player's position.
         if (targets != null)
         {
             if (followYAxis)
--- Assets/C#/ParallaxScenery.cs
+++ Assets/C#/ParallaxScenery.cs
@@ -6,6 +6,7 @@ public class ParallaxScenery : MonoBehaviour {
 
     GameObject[] targets;
     GameObject target;
+    // Multipliers that determine how much the scenery moves as the player moves.
     public float xMovementMultiplier = 1, yMovementMultiplier = 0;
     private float initX, initY, initZ;
     private float targetInitX, targetInitY;
@@ -13,8 +14,11 @@ public class ParallaxScenery : MonoBehaviour {
     // Use this for initialization
     void Start()
     {
+        // Determine the player to have the scenery "follow"
         targets = GameObject.FindGameObjectsWithTag("Player");
         target = targets[0];
+
+        // Get the initial position of the player and the scenery
         targetInitX = target.transform.position.x;
         targetInitY = target.transform.position.y;
         initX = this.transform.position.x;
@@ -25,6 +29,7 @@ public class ParallaxScenery : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        // Move the scenery based off of the change in the player's position
         if (targets != null)
         {
             transform.position = new Vector3(initX + xMovementMultiplier * (target.transform.position.x - targetInitX), initY + yMovementMultiplier * (target.transform.position.y - targetInitY), initZ);
