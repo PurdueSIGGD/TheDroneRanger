29ab279c3b6b4abd08f22542b23bb03fb692773b
Michael Beshear <MEBeshear@forted.com>
2018-10-04 19:31:49 -0400

    Added ColldownAbility
    Added the abstract class of having a cooldown

---
+++ Assets/C#/CooldownAbility.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Stolen shameless from Andrew Lonsway - edited by Michael Beshear
+public abstract class CooldownAbility
+{
+    /**
+     * Abstract class for repeated instances of cooldowns.
+     * You do not have to override the 'use' metod as a child of this, all you have to do is implement use_CooledDown
+     */
+    public enum AnimationType { Trigger, Bool, Float };
+    public AnimationType animationTriggerType = AnimationType.Trigger;
+    public string CAST_STRING = "Cast";
+
+    public float cooldown = 1; // Cooldown, in seconds
+    private float lastUse = -100; // Last time we used it, in seconds;
+    [HideInInspector]
+    public bool hasNotified;
+
+    public void ability_Start()
+    {
+        cooldown_Start();
+    }
+
+    public void use()
+    {
+        // Clients should not worry about magic draw
+        if (Time.time - lastUse > cooldown)
+        {
+                lastUse = Time.time;
+                hasNotified = false;
+                use_UseAbility();
+
+        }
+    }
+
+    public void ability_Update()
+    {
+        if (Time.time - lastUse > cooldown && !hasNotified)
+        {
+            use_CanUse();
+            hasNotified = true;
+        }
+        cooldown_Update();
+    }
+
+
+    /* these are the other methods you must implement. Can be empty, there for your own benefit */
+    public abstract void use_UseAbility(); // Called when the input says to use this ability
+    public abstract void cooldown_Start(); // Called when the object is alive
+    public abstract void cooldown_Update(); // Called once every frame
+    public abstract void use_CanUse(); // Called at the exact time the cooldown timer has reset
+}
+
---
+++ Assets/C#/ProjectileSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+class ProjectileSpawner : CooldownAbility
+{
+    public override void cooldown_Start()
+    {
+    }
+
+    public override void cooldown_Update()
+    {
+    }
+
+    public override void use_CanUse()
+    {
+    }
+
+    public override void use_UseAbility()
+    {
+        //TODO add targeting and spawn the bullet
+    }
+}
