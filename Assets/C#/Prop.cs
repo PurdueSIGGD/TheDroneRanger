41fb1d5b1bcafd37657a9009d173032083040444
Clayton <cjdetke@yahoo.com>
2018-11-13 19:16:15 -0500

    Destroyable crates now exist, and will be destroyed by bullets

--- Assets/C#/Prop.cs
+++ Assets/C#/Prop.cs
@@ -3,27 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Prop : MonoBehaviour {
-    public int durability = 1;
-    public bool destroyed = false; 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float durability = 1;
+    public Sprite destroyedSprite;
+
+    private SpriteRenderer myRenderer;
+    private Sprite defaultSprite;
+    // Use this for initialization
+    void Start ()
+    {
+        myRenderer = this.GetComponent<SpriteRenderer>();
+        defaultSprite = myRenderer.sprite;
+    }
+
+    // called to decrease the durability of a prop by amount
+    public void decreaseDurability(float amount)
+    {
+        print("hello!");
+        this.durability -= durability;
+        destroy();
+    }
 
     //called to change durability of a prop
-    void changeDurability(int durability) {
+    public void changeDurability(float durability) {
         this.durability = durability;
+        destroy();
     }
 
     // This should be called when the prop gets shot
     void destroy() {
-        durability--;
-        if(durability == 0)
-            destroyed = true;
+        if(isDestroyed())
+        {
+            myRenderer.sprite = destroyedSprite;
+        }
+    }
+
+    // This should be called to get whether or not this has been destroyed
+    public bool isDestroyed()
+    {
+        return this.durability <= 0;
     }
 }
