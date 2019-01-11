fa8964196a295c3051a16dfbe45eda4a0b35aafc
Michael Beshear <MEBeshear@forted.com>
2019-01-10 19:08:10 -0500

    HighNoon in progress
    Encountered bullet bug

--- Assets/C#/HighNoon.cs
+++ Assets/C#/HighNoon.cs
@@ -5,35 +5,36 @@ using UnityEngine;
 public class HighNoon : MonoBehaviour
 {
     public GameObject Player;
-    public GameObject Gun;
+    public ProjectileSpawner gun;
     public float activeTime;
 
-    private float charge = 0;
+    private float charge = 100;
     private int shotsLeft = 6;
     private bool active = false;
     private float startTime;
 
     private void startHighNoon()
     {
-        Debug.Log("active");
-        if (charge >= 100) return;
+        print("active");
+        //if (charge < 100) return;
         charge = 0;
         startTime = Time.realtimeSinceStartup;
-        Time.timeScale = 0.1f;
+        Time.timeScale = 0.02f;
         Time.fixedDeltaTime = 0.02f * Time.timeScale;
         active = true;
     }
 
     private void endHighNoon()
     {
+        shotsLeft = 6;
         Time.timeScale = 1.0f;
         Time.fixedDeltaTime = 0.02f * Time.timeScale;
         active = false;
     }
 
-    public void Update()
+    void Update()
     {
-        if(Input.GetAxis("Ability") != 0 || active)
+        if(Input.GetButton("Ability") || active)
         {
             if (!active)
             {
@@ -46,6 +47,15 @@ public class HighNoon : MonoBehaviour
             else
             {
                 //TODO add highnoon implimentation
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    gun.use_UseAbility();
+                    shotsLeft--;
+                    if(shotsLeft == 0)
+                    {
+                        endHighNoon();
+                    }
+                }
             }
         }
     }
