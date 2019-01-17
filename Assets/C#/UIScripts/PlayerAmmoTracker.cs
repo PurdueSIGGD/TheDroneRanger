6fef3a1d1a70595279f50563b7d6eb563b21970d
Clayton <cjdetke@yahoo.com>
2019-01-17 16:59:53 -0500

    Added ammo tracker UI

---
+++ Assets/C#/UIScripts/PlayerAmmoTracker.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerAmmoTracker : MonoBehaviour
+{
+    private const float fill = 1; // the ammo count's image can take any value from 0 to 1
+
+    private float fillAmount = 1;
+    private GameObject Player;
+    private PlayerAttributes stats;
+
+    [SerializeField]
+    private Image ammo;
+
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        stats = Player.GetComponent<PlayerAttributes>();
+    }
+
+    void Update()
+    {
+        updateHealth(stats.getAmmoCount(), 0, stats.maxAmmo);
+        ammo.fillAmount = fillAmount;
+    }
+
+    public void updateHealth(float newValue, float minValue, float maxValue)
+    {
+        float amount = (newValue - minValue) / (maxValue - minValue);
+        this.fillAmount = amount * fill;
+    }
+}
