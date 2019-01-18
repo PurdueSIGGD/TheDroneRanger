22455adde684de16757316857b710e650ab0000e
Landon Ellis <landon.ellis2@gmail.com>
2019-01-17 19:08:58 -0500

    Commit before Main branch pull

--- Assets/C#/PlayerAttributes.cs
+++ Assets/C#/PlayerAttributes.cs
@@ -2,11 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class PlayerAttributes : Attributes {
+
+    private List<GameObject> weapons = new List<GameObject>();
 
     //high noon at 100, should not be > 100
     public float highNoonPercent = 0;
+
+    public void Start()
+    {
+
+        WeaponAttributes[] preWeps = this.GetComponentsInChildren<WeaponAttributes>();
+        for (int i = 0; i < preWeps.Length; i++)
+        {
+            if (preWeps[i].gameObject == null)
+            {
+                continue;
+            }
+            weapons.Add(preWeps[i].gameObject);
+        }
+
+    }
+
+    public void addWeapon(GameObject wep)
+    {
+        weapons.Add(wep);
+    }
+
+    public List<GameObject> getWeaponList()
+    {
+        return weapons;
+    }
 
     public float getHighNoonPercent() {
         return highNoonPercent;
