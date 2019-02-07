0342ee8cde0c37a33a6970ef7e4b356bf8deeede
Clayton <cjdetke@yahoo.com>
2019-02-07 17:49:49 -0500

    Adding Looping scenery
    All that's left is to add enemies!!  (and fix bugs)

--- Assets/C#/Environment/LoopingBackground.cs
+++ Assets/C#/Environment/LoopingBackground.cs
@@ -4,23 +4,20 @@ using UnityEngine;
 
 public class LoopingBackground : MonoBehaviour {
 
-    private float initX, initY, initZ;
-    public float offsetX = -10;
+    public float initX = 10;
+    public float endX = 0;
     public float speed = 1;
 
     // Use this for initialization
     void Start () {
-        initX = this.transform.position.x;
-        initY = this.transform.position.y;
-        initZ = this.transform.position.z;
         this.GetComponent<Rigidbody2D>().velocity = new Vector2(-speed, 0);
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (this.transform.position.x < initX + offsetX)
+        if (this.transform.position.x < endX)
         {
-            this.transform.position = new Vector3(initX, initY, initZ);
+            this.transform.position = new Vector3(initX, this.transform.position.y, this.transform.position.z);
         }
 	}
 }
