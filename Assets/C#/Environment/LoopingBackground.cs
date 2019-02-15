73ae461a5f2385711bc684424417f928a8f47b86
Clayton <cjdetke@yahoo.com>
2019-02-14 19:38:14 -0500

    Actually fixed the looping scenery!!

--- Assets/C#/Environment/LoopingBackground.cs
+++ Assets/C#/Environment/LoopingBackground.cs
@@ -7,6 +7,8 @@ public class LoopingBackground : MonoBehaviour {
     public float initX = 10;
     public float endX = 0;
     public float speed = 1;
+    [SerializeField]
+    private GameObject camera;
 
     // Use this for initialization
     void Start () {
@@ -15,9 +17,9 @@ public class LoopingBackground : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (this.transform.position.x < endX)
+        if (this.transform.position.x < camera.transform.position.x + endX)
         {
-            this.transform.position = new Vector3(initX, this.transform.position.y, this.transform.position.z);
+            this.transform.position = new Vector3(initX + this.transform.position.x, this.transform.position.y, this.transform.position.z);
         }
 	}
 }
