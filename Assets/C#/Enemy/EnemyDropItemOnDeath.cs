1b16875d569ec7206886a48fead0ed3ff125d72c
Clayton <cjdetke@yahoo.com>
2019-03-01 12:05:10 -0500

    Made enemies able to "drop" items on death

---
+++ Assets/C#/Enemy/EnemyDropItemOnDeath.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDropItemOnDeath : MonoBehaviour
+{
+    // This is used for when an enemy spawns an item when they die.
+    public GameObject DropItem;
+    public Vector3 offset;
+    
+    public void spawnItem()
+    {
+        GameObject item = GameObject.Instantiate(DropItem);
+        item.transform.position = this.transform.position + offset;
+    }
+}
