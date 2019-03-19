e79c2bd0b8e9345679d6001e1cf63dda3ea7c9ea
Clayton <cjdetke@yahoo.com>
2019-03-19 14:24:43 -0400

    Added credits and quit buttons

--- Assets/C#/UIScripts/MainMenu.cs
+++ Assets/C#/UIScripts/MainMenu.cs
@@ -14,4 +14,10 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void Exit()
+    {
+        Debug.Log("The game has now closed."); // This is only done for when testing in the unity editor
+        Application.Quit(); // This will close the program, when it is built, NOT when it is in the unity editor.
+    }
 }
