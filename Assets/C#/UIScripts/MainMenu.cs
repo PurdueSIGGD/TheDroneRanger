93db6d8c84ba7006104ba5a63994bc0d60580ad3
Clayton <cjdetke@yahoo.com>
2019-03-19 14:14:11 -0400

    Created basic main menu with start button

---
+++ Assets/C#/UIScripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartButton()
+    {
+        SceneManager.LoadScene("Scenes/Tutorial");
+    }
+
+    public void NextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
