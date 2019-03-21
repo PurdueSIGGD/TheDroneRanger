81375030e113921ecfd9631cd66a2987586243c8
andyw24 <andyw24@gmail.com>
2019-03-21 18:48:06 -0400

    added lasers (red)

---
+++ Assets/C#/Environment/LaserTraps.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserTraps : MonoBehaviour
+{
+	[Tooltip("All times are in seconds\nDefault times are 1 second")]
+	public float laserLength;
+	public float onTime;
+	public float offTime;
+	public bool random;
+	public bool randomMaxTime;
+	public bool randomMinTime;
+	private GameObject laser;
+
+	void Start()
+    {
+		laser = transform.GetChild(0).gameObject;
+		laser.transform.localScale = new Vector3(1f, laserLength, 0f);
+		laser.transform.localPosition = new Vector3(0f, 0.32f * laserLength - 0.05f, 0f);
+		if (onTime <= 0)
+		{
+			onTime = 1;
+		}
+		if (offTime <= 0)
+		{
+			offTime = 1;
+		}
+		StartCoroutine(LaserTrapOn());
+	}
+
+    IEnumerator LaserTrapOn()
+	{
+		while (true)
+		{
+			laser.SetActive(true);
+			yield return new WaitForSeconds(onTime);
+			laser.SetActive(false);
+			yield return new WaitForSeconds(offTime);
+		}
+	}
+}
---
+++ Assets/C#/LaserDamage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserDamage : MonoBehaviour
+{
+	public float damage;
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (damage <= 0)
+		{
+			Debug.Log("Laser damage value is <= 0");
+		} else
+		{
+			if (collision.gameObject.name.Equals("Player"))
+			{
+				collision.gameObject.GetComponent<PlayerAttributes>().takeDamage(damage);
+			}
+		}
+	}
+}
