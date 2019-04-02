dd078010b6c957a1989fdc2ad080c58f232533a6
Landon Ellis <landon.ellis2@gmail.com>
2019-04-02 19:31:16 -0400

    BossFight#1

--- Assets/C#/Environment/BossTrigger.cs
+++ Assets/C#/Environment/BossTrigger.cs
@@ -10,19 +10,39 @@ class BossTrigger : MonoBehaviour
     public List<Collider2D> bounds = new List<Collider2D>();
     public float panDuration = 1.0f;
     private BossHealthBar healthBar = null;
+    private PlayerAttributes player = null;
+    private bool triggered = false;
 
-    void Start()
+    protected void Start()
     {
         healthBar = GameObject.FindGameObjectWithTag("UI").GetComponentInChildren<BossHealthBar>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerAttributes>();
         for (int i = 0; i < bounds.Count; i++)
         {
             bounds[i].isTrigger = true;
         }
     }
 
+    protected virtual void OnBossFightEnd() { }
+
+    void Update()
+    {
+        if (player.getHealth() == 0 || boss.getHealth() == 0)
+        {
+            healthBar.setBoss(null);
+            healthBar.transform.parent.gameObject.SetActive(false);
+            for (int i = 0; i < bounds.Count; i++)
+            {
+                bounds[i].isTrigger = true;
+            }
+            OnBossFightEnd();
+            Destroy(this.gameObject);
+        }
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (!boss || !healthBar)
+        if (triggered || !boss || !healthBar)
         {
             return;
         }
@@ -38,6 +58,6 @@ class BossTrigger : MonoBehaviour
 
         healthBar.setBoss(boss);
         healthBar.transform.parent.gameObject.SetActive(true);
-        Destroy(this.gameObject);
+        triggered = true;
     }
 }
---
+++ Assets/C#/Environment/TutorialBoss.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+class TutorialBoss : BossTrigger
+{
+    public GameObject endCamCenter = null;
+    private string EXPLOSION_PATH = "Environment/Explosion_Particles";
+
+   void Start()
+    {
+        base.Start();
+    }
+
+    protected override void OnBossFightEnd()
+    {
+        CameraControl cam = Camera.main.GetComponent<CameraControl>();
+        cam.Pan(endCamCenter.transform.position, panDuration);
+        cam.zoom(0.85f);
+        createExplosion().transform.position = endCamCenter.transform.position;
+    }
+
+    GameObject createExplosion()
+    {
+        return (Instantiate(Resources.Load(EXPLOSION_PATH, typeof(GameObject))) as GameObject);
+    }
+
+}
