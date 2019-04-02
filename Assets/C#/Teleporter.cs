53061ba88a88ef723fba21fbc6ca8b7f28fc70cf
andyw24 <andyw24@gmail.com>
2019-04-02 18:50:20 -0400

    added teleporter prefab
    - 2 teleporter prefabs are needed in the scene to teleport
    - one way teleporting is possible, just leave the correspondingTeleporter null for the destination teleporter
    - just using a temporary teleporter asset from google images for now

---
+++ Assets/C#/Teleporter.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+	/*
+	 * A second teleporter prefab needs to be in the scene for this teleporter to work
+	 */
+
+	public GameObject correspondingTeleporter;
+	private Vector2 teleportTo;
+	private bool canTeleport; //player can only teleport again if they exit the teleporter
+
+    void Start()
+    {
+		canTeleport = true;
+		if (correspondingTeleporter.GetComponent<Teleporter>().correspondingTeleporter != null && correspondingTeleporter.GetComponent<Teleporter>().correspondingTeleporter != gameObject)
+		{
+			Debug.LogError(this.name + "'s corresponding teleporter doesn't match");
+		}
+		else
+		{
+			teleportTo = correspondingTeleporter.transform.position;
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		GameObject player = collision.gameObject;
+		if (canTeleport && player.name.Equals("Player"))
+		{
+			if (correspondingTeleporter != null)
+			{
+				player.transform.position = teleportTo;
+				correspondingTeleporter.GetComponent<Teleporter>().setCanTeleport(false);
+			} 
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		GameObject player = collision.gameObject;
+		if (player.name.Equals("Player"))
+		{
+			canTeleport = true;
+		}
+	}
+
+	public void setCanTeleport(bool b)
+	{
+		canTeleport = b;
+	}
+}
