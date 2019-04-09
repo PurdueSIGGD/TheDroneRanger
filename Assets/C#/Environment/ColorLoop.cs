942e656099cb012ea368ceac89ed0dcfa99ea17b
Michael Beshear <MEBeshear@forted.com>
2019-04-09 19:39:58 -0400

    Added Color Looping and spruced up backgrounds
    Added ability for a sprite to gently shift from one color to another, made backgrounds look better.

---
+++ Assets/C#/Environment/ColorLoop.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorLoop : MonoBehaviour
+{
+    private SpriteRenderer sprite;
+    private Color startColor;
+    public Color endColor;
+    public float duration; //Time between the start and ending colors, not cycle time
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        startColor = sprite.color;
+        endColor.a = 1;
+        StartCoroutine("ColorShift");
+    }
+    
+    IEnumerator ColorShift()
+    {
+        while (true)
+        {
+            for (float i = 0; i < duration; i += duration / 10)
+            {
+                Color newColor = ((startColor) * (duration - i)) + ((endColor) * (i));
+                newColor /= duration;
+                sprite.color = newColor;
+                yield return new WaitForSeconds(duration / 10);
+            }
+            for(float i = duration; i > 0; i -= duration / 10)
+            {
+                Color newColor = ((startColor) * (duration - i)) + ((endColor) * (i));
+                newColor /= duration;
+                sprite.color = newColor;
+                yield return new WaitForSeconds(duration / 10);
+            }
+        }
+    }
+}
