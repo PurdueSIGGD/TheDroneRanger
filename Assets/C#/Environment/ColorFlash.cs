8492bc5f296d1080080853171255f99decb386bf
Michael Beshear <MEBeshear@forted.com>
2019-04-11 19:23:57 -0400

    Updated Sprites, Added ColorFlash
    Updated sprites of the door and key.
    Added ColorFlash script to have elements flash colors briefly.

---
+++ Assets/C#/Environment/ColorFlash.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorFlash : MonoBehaviour
+{
+    private SpriteRenderer sprite;
+    private Color flashColor;
+    public Color usualColor;
+    public float waitTime; //Time each flash of color
+    public float flashTime; //How long a color flash lasts
+    public float offset = 0; //Offset on intial flash, should be > 0 but < WaitTime
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        flashColor = sprite.color;
+        usualColor.a = 1;
+        StartCoroutine("ColorFlash");
+    }
+
+    IEnumerator ColorFlash()
+    {
+        sprite.color = usualColor;
+        yield return new WaitForSeconds(waitTime - offset);
+        sprite.color = flashColor;
+        yield return new WaitForSeconds(flashTime);
+        while (true)
+        {
+            sprite.color = usualColor;
+            yield return new WaitForSeconds(waitTime);
+            sprite.color = flashColor;
+            yield return new WaitForSeconds(flashTime);
+        }
+    }
+}
--- Assets/C#/Environment/ColorLoop.cs
+++ Assets/C#/Environment/ColorLoop.cs
@@ -8,6 +8,9 @@ public class ColorLoop : MonoBehaviour
     private Color startColor;
     public Color endColor;
     public float duration; //Time between the start and ending colors, not cycle time
+    public float offset; //Offset on intial loop, should be > 0 but < 2*duration
+    public bool highFidelity; //Used to increase the number of steps between two colors, more CPU intensive but looks better.
+    public int fidelity; //Custom Fidelity to use with HighFidelity
 
     // Start is called before the first frame update
     void Start()
@@ -15,11 +18,45 @@ public class ColorLoop : MonoBehaviour
         sprite = GetComponent<SpriteRenderer>();
         startColor = sprite.color;
         endColor.a = 1;
-        StartCoroutine("ColorShift");
+        if (highFidelity) StartCoroutine("HighColorShift");
+        else StartCoroutine("ColorShift");
     }
     
     IEnumerator ColorShift()
     {
+        if(offset != 0)
+        {
+            if(offset < duration)
+            {
+                for (float i = offset; i < duration; i += duration / 10)
+                {
+                    Color newColor = ((startColor) * (duration - i)) + ((endColor) * (i));
+                    newColor /= duration;
+                    sprite.color = newColor;
+                    yield return new WaitForSeconds(duration / 10);
+                }
+            }
+            if(offset > duration)
+            {
+                for (float i = offset - duration; i > 0; i -= duration / 10)
+                {
+                    Color newColor = ((startColor) * (duration - i)) + ((endColor) * (i));
+                    newColor /= duration;
+                    sprite.color = newColor;
+                    yield return new WaitForSeconds(duration / 10);
+                }
+            }
+            else
+            {
+                for (float i = duration; i > 0; i -= duration / 10)
+                {
+                    Color newColor = ((startColor) * (duration - i)) + ((endColor) * (i));
+                    newColor /= duration;
+                    sprite.color = newColor;
+                    yield return new WaitForSeconds(duration / 10);
+                }
+            }
+        }
         while (true)
         {
             for (float i = 0; i < duration; i += duration / 10)
@@ -35,6 +72,60 @@ public class ColorLoop : MonoBehaviour
                 newColor /= duration;
                 sprite.color = newColor;
                 yield return new WaitForSeconds(duration / 10);
+            }
+        }
+    }
+
+    IEnumerator HighColorShift()
+    {
+        if (offset != 0)
+        {
+            if (offset < duration)
+            {
+                for (float i = offset; i < duration; i += duration / fidelity)
+                {
+                    Color newColor = ((startColor) * (duration - i)) + ((endColor) * (i));
+                    newColor /= duration;
+                    sprite.color = newColor;
+                    yield return new WaitForSeconds(duration / fidelity);
+                }
+            }
+            if (offset > duration)
+            {
+                for (float i = offset - duration; i > 0; i -= duration / fidelity)
+                {
+                    Color newColor = ((startColor) * (duration - i)) + ((endColor) * (i));
+                    newColor /= duration;
+                    sprite.color = newColor;
+                    yield return new WaitForSeconds(duration / fidelity);
+                }
+            }
+            else
+            {
+                for (float i = duration; i > 0; i -= duration / fidelity)
+                {
+                    Color newColor = ((startColor) * (duration - i)) + ((endColor) * (i));
+                    newColor /= duration;
+                    sprite.color = newColor;
+                    yield return new WaitForSeconds(duration / fidelity);
+                }
+            }
+        }
+        while (true)
+        {
+            for (float i = 0; i < duration; i += duration / fidelity)
+            {
+                Color newColor = ((startColor) * (duration - i)) + ((endColor) * (i));
+                newColor /= duration;
+                sprite.color = newColor;
+                yield return new WaitForSeconds(duration / fidelity);
+            }
+            for (float i = duration; i > 0; i -= duration / fidelity)
+            {
+                Color newColor = ((startColor) * (duration - i)) + ((endColor) * (i));
+                newColor /= duration;
+                sprite.color = newColor;
+                yield return new WaitForSeconds(duration / fidelity);
             }
         }
     }
