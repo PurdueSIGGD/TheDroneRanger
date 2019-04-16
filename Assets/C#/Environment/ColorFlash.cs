daba6ad96d73ee81c46366136e028ece91516106
Michael Beshear <MEBeshear@forted.com>
2019-04-16 19:45:19 -0400

    Fully added Control Panels to Background
    Added Controls Panels that flash lights in the background

--- Assets/C#/Environment/ColorFlash.cs
+++ Assets/C#/Environment/ColorFlash.cs
@@ -5,32 +5,34 @@ using UnityEngine;
 public class ColorFlash : MonoBehaviour
 {
     private SpriteRenderer sprite;
-    private Color flashColor;
-    public Color usualColor;
-    public float waitTime; //Time each flash of color
+    public Color[] flashColors = { Color.white };
+    public Color usualColor = Color.white;
+    public float waitTimeLower; //Minimum time for each flash of color
+    public float waitTimeUpper; //Maximum time for each falsh of color
     public float flashTime; //How long a color flash lasts
-    public float offset = 0; //Offset on intial flash, should be > 0 but < WaitTime
+    public float offset = 0; //Offset on intial flash, should be > 0 but < WaitTimeLower
 
     // Start is called before the first frame update
     void Start()
     {
         sprite = GetComponent<SpriteRenderer>();
-        flashColor = sprite.color;
-        usualColor.a = 1;
         StartCoroutine("Flash");
     }
 
     IEnumerator Flash()
     {
-        sprite.color = usualColor;
-        yield return new WaitForSeconds(waitTime - offset);
-        sprite.color = flashColor;
-        yield return new WaitForSeconds(flashTime);
+        if (offset != 0)
+        {
+            sprite.color = usualColor;
+            yield return new WaitForSeconds(waitTimeLower - offset);
+            sprite.color = flashColors[(int)Random.Range(0, flashColors.Length + 0.01f)];
+            yield return new WaitForSeconds(flashTime);
+        }
         while (true)
         {
             sprite.color = usualColor;
-            yield return new WaitForSeconds(waitTime);
-            sprite.color = flashColor;
+            yield return new WaitForSeconds(Random.Range(waitTimeLower, waitTimeUpper));
+            sprite.color = flashColors[(int)Random.Range(0, flashColors.Length - 0.01f)];
             yield return new WaitForSeconds(flashTime);
         }
     }
